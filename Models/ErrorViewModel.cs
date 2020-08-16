85223593bd7a91ae6864a659ab6ad6d3f9b5ae14
stat-ki <per_ik@outlook.com>
2020-08-16 12:27:37 +0900

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcBasic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
